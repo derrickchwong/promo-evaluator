9edbacb5dbeef465ad4a8822fba8caeb83185763
Derrick Wong <derrick.chwong@gmail.com>
2024-01-03 07:49:04 +0000

    add missing config

--- src/main/java/com/example/promoevaluator/config/MongoConfig.java
+++ src/main/java/com/example/promoevaluator/config/MongoConfig.java
@@ -3,6 +3,7 @@ package com.example.promoevaluator.config;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 import com.mongodb.ConnectionString;
@@ -10,6 +11,7 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
+@Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
     
     @Override
@@ -19,7 +21,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
  
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://mongo-mongo-db:27017/test");
+        ConnectionString connectionString = new ConnectionString("mongodb://mongo-mongodb:27017/test");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .build();
