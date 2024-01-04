048ef264069c0de4190e0b3ef412c80d5f7a32b1
Derrick Wong <derrick.chwong@gmail.com>
2024-01-04 09:25:48 +0000

    fix test

--- src/test/java/com/example/promoevaluator/service/PromoEvaluatorTest.java
+++ src/test/java/com/example/promoevaluator/service/PromoEvaluatorTest.java
@@ -7,95 +7,141 @@ import com.example.promoevaluator.model.Order;
 import com.example.promoevaluator.model.OrderItem;
 import com.example.promoevaluator.model.Product;
 import com.example.promoevaluator.model.ProductGroup;
+import com.example.promoevaluator.repo.CustomerRepository;
+import com.example.promoevaluator.repo.OrderRepository;
+import com.example.promoevaluator.repo.ProductGroupRepository;
+import com.example.promoevaluator.repo.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class PromoEvaluatorTest {
 
-    // @Test
-    // public void whenNoExistingAvailableCampaign_thenAvailableCampaignIsCreated() {
 
-    //     // Create a customer
-    //     Customer customer = Customer.builder().id("1").build();
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private ProductGroupRepository productGroupRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private PromoEvaluator promoEvaluator;
+    
+    @Test
+    public void whenNoExistingAvailableCampaign_thenAvailableCampaignIsCreated() {
+
+        // Create a customer
+        Customer customer = Customer.builder().id("1").build();
         
-    //     // Create an order
-    //     Order order = Order.builder().customerId(customer.getId()).build();
+        // Create an order
+        Order order = Order.builder().customerId(customer.getId()).build();
 
-    //     ProductGroup pg1 = ProductGroup.builder().id("pg1").build();
+        ProductGroup pg1 = ProductGroup.builder().id("pg1").build();
         
-    //     Product p1 = Product.builder().id("p1").productGroupId(pg1.getId()).build();
+        Product p1 = Product.builder().id("p1").productGroupId(pg1.getId()).build();
 
-    //     pg1.setProducts(List.of(p1));
+        pg1.setProducts(List.of(p1));
 
-    //     Merchant merchant1 = Merchant.builder().id("merchant1").build();
+        Merchant merchant1 = Merchant.builder().id("merchant1").build();
         
-    //     Campaign campaign1 = Campaign.builder().id("campaign1").merchantId(merchant1.getId()).build();
+        Campaign campaign1 = Campaign.builder().id("campaign1").merchantId(merchant1.getId()).build();
+        merchant1.addCampaign(campaign1);
         
-    //     // pg1.setCampaigns(List.of(campaign1));
-    //     campaign1.setProductGroupAmountMap(Map.of(pg1.getId(), 100));
-
-    //     // Create an order item
-    //     OrderItem item1 = OrderItem.builder().productId(p1.getId()).price(10).build();
-
-    //     // Add the order item to the order
-    //     order.addOrderItem(item1);
-
-    //     // Evaluate the order
-    //     PromoEvaluator promoEvaluator = new PromoEvaluator();
-    //     Customer updatedCustomer = promoEvaluator.orderReceiver(order);
-
-    //     // Assert that the customer's available campaign has been updated
-    //     assertTrue(updatedCustomer.getAvailableCampaigns().containsKey(campaign1));
-    //     assertEquals(90, updatedCustomer.getAvailableCampaigns().get(campaign1));
-
-    // }
+        // pg1.setCampaigns(List.of(campaign1));
+        campaign1.setProductGroupAmountMap(Map.of(pg1.getId(), 100));
+        pg1.addCampaign(campaign1);
 
 
-    // @Test
-    // public void whenExistingAvailableCampaign_thenAvailableCampaignIsUpdated() {
+        // Create an order item
+        OrderItem item1 = OrderItem.builder().productId(p1.getId()).price(10).build();
 
-    //     // Create a customer
-    //     Customer customer = Customer.builder().id("1").build();
+        // Add the order item to the order
+        order.addOrderItem(item1);
 
-    //     // Create an order
-    //     Order order = Order.builder().customer(customer).build();
+
+        when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
+        when(productGroupRepository.findById(pg1.getId())).thenReturn(Optional.of(pg1));
+        when(productRepository.findById(p1.getId())).thenReturn(Optional.of(p1));
+        when(orderRepository.save(order)).thenReturn(order);
+        when(customerRepository.save(customer)).thenReturn(customer);
+
+
+        // Evaluate the order
+        Customer updatedCustomer = promoEvaluator.orderReceiver(order);
+
+        // Assert that the customer's available campaign has been updated
+        assertNotNull(updatedCustomer.getAvailableCampaigns());
+        assertTrue(updatedCustomer.getAvailableCampaigns().containsKey(campaign1.getId()));
+        assertEquals(90, updatedCustomer.getAvailableCampaigns().get(campaign1.getId()));
+
+    }
+
+
+    @Test
+    public void whenExistingAvailableCampaign_thenAvailableCampaignIsUpdated() {
+
+        // Create a customer
+        Customer customer = Customer.builder().id("1").build();
+
+        // Create an order
+        Order order = Order.builder().customerId(customer.getId()).build();
         
-    //     ProductGroup pg1 = ProductGroup.builder().id("pg1").build();
+        ProductGroup pg1 = ProductGroup.builder().id("pg1").build();
         
-    //     Product p1 = Product.builder().id("p1").productGroup(pg1).build();
+        Product p1 = Product.builder().id("p1").productGroupId(pg1.getId()).build();
         
-    //     pg1.setProducts(List.of(p1));
+        pg1.setProducts(List.of(p1));
 
-    //     Merchant merchant1 = Merchant.builder().id("merchant1").build();
+        Merchant merchant1 = Merchant.builder().id("merchant1").build();
         
-    //     Campaign campaign1 = Campaign.builder().id("campaign1").merchant(merchant1).build();
-        
-    //     // pg1.setCampaigns(List.of(campaign1));
-    //     campaign1.setProductGroupAmountMap(Map.of(pg1, 100));
+        Campaign campaign1 = Campaign.builder().id("campaign1").merchantId(merchant1.getId()).build();
 
-    //     // Create an order item
-    //     OrderItem item1 = OrderItem.builder().product(p1).price(10).build();
+        merchant1.addCampaign(campaign1);
         
-    //     // Add the order item to the order
-    //     order.addOrderItem(item1);
+        // pg1.setCampaigns(List.of(campaign1));
+        campaign1.setProductGroupAmountMap(Map.of(pg1.getId(), 100));
+        pg1.addCampaign(campaign1);
 
-    //     // Update the customer's available campaign
-    //     customer.updateAvailableCampaign(campaign1, 90);
+
+        // Create an order item
+        OrderItem item1 = OrderItem.builder().productId(p1.getId()).price(10).build();
+        
+        // Add the order item to the order
+        order.addOrderItem(item1);
+
+        // Update the customer's available campaign
+        customer.updateAvailableCampaign(campaign1, 90);
    
-    //     // Evaluate the order
-    //     PromoEvaluator promoEvaluator = new PromoEvaluator();
-    //     Customer updatedCustomer = promoEvaluator.orderReceiver(order);
 
-    //     // Assert that the customer's available campaign has been updated
-    //     assertTrue(updatedCustomer.getAvailableCampaigns().containsKey(campaign1));
-    //     assertEquals(80, updatedCustomer.getAvailableCampaigns().get(campaign1));
+        when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
+        when(productGroupRepository.findById(pg1.getId())).thenReturn(Optional.of(pg1));
+        when(productRepository.findById(p1.getId())).thenReturn(Optional.of(p1));
+        when(orderRepository.save(order)).thenReturn(order);
+        when(customerRepository.save(customer)).thenReturn(customer);
 
-    // }
+        // Evaluate the order
+        Customer updatedCustomer = promoEvaluator.orderReceiver(order);
+
+        // Assert that the customer's available campaign has been updated
+        assertNotNull(updatedCustomer.getAvailableCampaigns());
+        assertTrue(updatedCustomer.getAvailableCampaigns().containsKey(campaign1.getId()));
+        assertEquals(80, updatedCustomer.getAvailableCampaigns().get(campaign1.getId()));
+
+    }
 
 
     // @Test
@@ -150,7 +196,6 @@ public class PromoEvaluatorTest {
     //     customer.updateAvailableCampaign(campaign2, 90);
 
     //     // Evaluate the order
-    //     PromoEvaluator promoEvaluator = new PromoEvaluator();
     //     Customer updatedCustomer = promoEvaluator.orderReceiver(order);
 
     //     // Assert that the customer's available campaign has been updated
