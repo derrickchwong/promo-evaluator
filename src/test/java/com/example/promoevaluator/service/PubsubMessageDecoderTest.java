f41958ccae9c800632c55f2fa41ab473d2b49d6d
Derrick Wong <derrick.chwong@gmail.com>
2024-01-10 04:21:29 +0000

    polish

--- src/test/java/com/example/promoevaluator/service/PubsubMessageDecoderTest.java
+++ src/test/java/com/example/promoevaluator/service/PubsubMessageDecoderTest.java
@@ -47,10 +47,12 @@ public class PubsubMessageDecoderTest {
         //     "QTY": 1
         // }
 
+        String json = "{\"DATAFLAG\":\"1\",\"CUST_NO\":\"201902501293\",\"ECM_PROMO_NO\":\"20240105142141577\",\"START_DATE\":\"2024/01/05 15:00:00\",\"END_DATE\":\"2024/01/30 15:01:00\",\"M_PROMO_NO\":\"M24010500124\",\"REMOVE_FLAG\":\"1\",\"ORDER_NO\":\"24010561108493\",\"FULL_ORDERNO\":\"24010561108493-001-001-001\",\"ORDER_DATE\":\"2024/01/05 15:32:02\",\"SET_YN\":\"0\",\"GOODS_CODE\":\"5770284\",\"GOODSDT_CODE\":\"001\",\"GOODS_NAME\":\"【ZOJIRUSHI 象印】SLiT不鏽鋼真空保溫瓶保溫杯820ml(SJ-JS08)\",\"GOODSDT_INFO\":\"紅色(RA) 820cc\",\"RSALE_AMT\":1290,\"QTY\":1}";
+
         // Create a Pub/Sub message.
         Body body = new Body();
         Message message = body.new Message();
-        message.setData(Base64.getEncoder().encodeToString("{\"DATAFLAG\": \"1\", \"CUST_NO\": \"201902501293\", \"ECM_PROMO_NO\": \"20240105142141577\", \"START_DATE\": \"2024/01/05 15:00:00\", \"END_DATE\": \"2024/01/30 15:01:00\", \"M_PROMO_NO\": \"M24010500124\", \"REMOVE_FLAG\": \"1\", \"ORDER_NO\": \"24010561108493\", \"FULL_ORDERNO\": \"24010561108493-001-001-001\", \"ORDER_DATE\": \"2024/01/05 15:32:02\", \"SET_YN\": \"0\", \"GOODS_CODE\": \"5770284\", \"GOODSDT_CODE\": \"001\", \"GOODS_NAME\": \"【ZOJIRUSHI 象印】SLiT不鏽鋼真空保溫瓶保溫杯820ml(SJ-JS08)\", \"GOODSDT_INFO\": \"紅色(RA) 820cc\", \"RSALE_AMT\": 1290, \"QTY\": 1}".getBytes()));
+        message.setData(Base64.getEncoder().encodeToString(json.getBytes()));
         // Create a Body object.
         body.setMessage(message);
         
